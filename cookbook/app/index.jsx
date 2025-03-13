201167777310cf93f5adfa5dd94d6e08b20ea735
sadsewd <lacitiskarlis@gmail.com>
2025-03-13 17:31:50 +0200

    Added routing, context, basic login logic

--- cookbook/app/index.jsx
+++ cookbook/app/index.jsx
@@ -1,20 +1,50 @@
-import { Link } from 'expo-router'
+import { Link, Redirect } from 'expo-router'
 import { StatusBar } from 'expo-status-bar'
 import { StyleSheet, Text, View } from 'react-native'
+import { useGlobalContext } from '../context/GlobalProvider'
 
 export default function App() {
+  const { isLogged } = useGlobalContext()
+
+  if (isLogged) return <Redirect href='/recipes' />
+
   return (
-    <View style={styles.container}>
-      <Link href='/recipes'>Open up App.js to start working on your app!</Link>
+    <>
       <StatusBar style='auto' />
-    </View>
+      <View style={styles.container}>
+        <View style={styles.buttonsCon}>
+          <Link href='/login' style={styles.buttonCon}>
+            <Text>Login</Text>
+          </Link>
+          <Link href='/register' style={styles.buttonCon}>
+            <Text>Register</Text>
+          </Link>
+        </View>
+      </View>
+    </>
   )
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    height: '100vh',
+    width: '100vw',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  buttonsCon: {
+    width: '70%',
+    height: '50%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'red',
+    gap: '5%',
+  },
+  buttonCon: {
+    width: '90%',
+    height: '40%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'green',
   },
 })
