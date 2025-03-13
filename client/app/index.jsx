399bbf2b1a9f1c05b97462537193fb2ac5a8e9d7
Endijs <endijsdarznieks@inbox.lv>
2025-03-13 19:20:35 +0200

    axios

--- client/app/index.jsx
+++ client/app/index.jsx
@@ -1,12 +1,23 @@
 import { Link } from 'expo-router'
 import { StatusBar } from 'expo-status-bar'
 import { StyleSheet, Text, View } from 'react-native'
+import axios from 'axios'
+import { useEffect } from 'react'
 
 export default function App() {
+  useEffect(() => {
+    axios
+      .post('http://localhost:3001/auth/login', {
+        email: 'tt',
+        password: 'Parole123',
+      })
+      .then((res) => console.log(res))
+  }, [])
+
   return (
     <View style={styles.container}>
-      <Link href='/recipes'>Open up App.js to start working on your app!</Link>
       <StatusBar style='auto' />
+      <Link href='/recipes'>Open up App.js to start working on your app!</Link>
     </View>
   )
 }
--- server/auth.js
+++ server/auth.js
@@ -7,4 +7,76 @@ import jwt from 'jsonwebtoken'
 
 const router = express.Router()
 
+// router.post('/login', async (req, res) => {
+//   const { email, password } = req.body
+
+//   db.query(`SELECT * FROM users WHERE email = ?`, email, (err, result) => {
+//     if (err) res.status(500).json({ message: err.message })
+//     else {
+//       if (result.length < 1) {
+//         res.send({ message: 'Invalid credentials' })
+//         return
+//       }
+//       if (result.length > 1) {
+//         res.send({ message: 'Duplicate accounts' })
+//         return
+//       }
+
+//       const user = result[0]
+//       const isCorrectPass = bcrypt.compareSync(password, user.password)
+
+//       if (isCorrectPass) {
+//         // const tokens = updateTokens(user)
+//         res.cookie('session', 'dark_theme', {
+//           maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
+//           httpOnly: true,
+//           sameSite: 'strict',
+//         })
+//         res.sendStatus(200)
+//       } else res.send({ message: 'Invalid credentials' })
+//     }
+//   })
+// })
+
+// const updateTokens = (user) => {
+//   const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET, {
+//     expiresIn: '30s',
+//   })
+
+//   db.query(
+//     `UPDATE users SET refresh_token = ? WHERE users_id = ?`,
+//     [refreshToken, user.users_id],
+//     (err) => {
+//       if (err) {
+//         res.status(500).json({ message: err.message })
+//       } else {
+//         const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+//           httpOnly: true,
+//           sameSite,
+//         })
+
+//         res.send({ message: 'Updated entry: ' + id })
+//       }
+//     }
+//   )
+
+//   res.send({
+//     accessToken: accessToken,
+//   })
+// }
+
+// router.post('/logout', async (req, res) => {
+//   db.query(
+//     `UPDATE users SET refresh_token = null WHERE refresh_token = ?`,
+//     req.body.refreshToken,
+//     (err) => {
+//       if (err) {
+//         res.status(500).json({ message: err.message })
+//       } else {
+//         res.send({ message: 'Logged out' })
+//       }
+//     }
+//   )
+// })
+
 export default router
